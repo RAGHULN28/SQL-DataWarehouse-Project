4d79e8a14315828ff56b822443d56a9a3d62f943
RAGHUL N <139314050+RAGHULN28@users.noreply.github.com>
2026-05-17 11:54:43 +0530

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'.

---
+++ CodeFiles/init_database.sql
@@ -0,0 +1,26 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' and then it sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold' */
+
+USE master;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA GOLD;
